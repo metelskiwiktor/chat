59c86bffc59148dd012f755c7f90ba70c77a5f1b
WascinskiM <marcin.wasc@gmail.com>
2021-02-26 17:17:46 +0100

    Add new route to project

--- frontend/client/src/app/app-routing.module.ts
+++ frontend/client/src/app/app-routing.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MockComponent} from './mock/mock.component';
+import {PrivateMessagesComponent} from './private-messages/private-messages.component';
+import {ProfileComponent} from './profile/profile.component';
+import {RoomComponent} from './room/room.component';
+import {RoomsComponent} from './rooms/rooms.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'mock', pathMatch: 'full' },
   { path: 'mock', component: MockComponent },
+  { path: 'private-messages', component: PrivateMessagesComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'room', component: RoomComponent },
+  { path: 'rooms', component: RoomsComponent },
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(
-    [
-      { path: 'mock', component: MockComponent}
-    ]
-  )],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [PrivateMessagesComponent, ProfileComponent, RoomsComponent, RoomComponent];
