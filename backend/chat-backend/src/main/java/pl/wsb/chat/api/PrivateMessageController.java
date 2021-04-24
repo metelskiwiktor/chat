85fd1e16fe0f64873cce77e2945faf48e930c26d
metelskiwiktor <metelski.wiktor@gmail.com>
2021-04-24 23:47:12 +0200

    Updated private message controller

--- backend/chat-backend/src/main/java/pl/wsb/chat/api/PrivateMessageController.java
+++ backend/chat-backend/src/main/java/pl/wsb/chat/api/PrivateMessageController.java
@@ -23,21 +23,8 @@ public class PrivateMessageController {
         privateMessageService.addMessage(sendPrivateMessageRequest);
     }
 
-//    @GetMapping("/conversation")
-//    public List<PrivateMessage> getPrivateMessages(@RequestParam("recipient-id") String recipientId) {
-//        return privateMessageService.getMessages(recipientId);
-//    }
-
     @GetMapping("/conversations")
     public List<PrivateMessageView> getConversations() {
-        PrivateMessageView privateMessageView1 = new PrivateMessageView("1234rec", "asd", "wiadomosc", LocalDateTime.now());
-        PrivateMessageView privateMessageView2 = new PrivateMessageView("1234rec", "asd", "zxczx", LocalDateTime.now());
-        PrivateMessageView privateMessageView3 = new PrivateMessageView("1234rec", "asd", "bcv", LocalDateTime.now());
-        PrivateMessageView privateMessageView4 = new PrivateMessageView("1234rec", "asd", "zxc", LocalDateTime.now());
-        PrivateMessageView privateMessageView5 = new PrivateMessageView("3453", "asd", "axdasd", LocalDateTime.now());
-        PrivateMessageView privateMessageView6 = new PrivateMessageView("12312", "asd", "asdaz", LocalDateTime.now());
-
-        return Arrays.asList(privateMessageView1, privateMessageView2, privateMessageView3, privateMessageView4, privateMessageView5, privateMessageView6);
-//        return privateMessageService.getConversations();
+        return privateMessageService.getConversations();
     }
 }
